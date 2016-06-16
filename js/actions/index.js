53f058eba62fb64471d673dd96a4c0280d59d773
IT-kenlee(李昆芳) <markuslee0502@gmail.com>
2016-06-16 09:33:39 +0800

    Fetch stock information from own server.

--- js/actions/index.js
+++ js/actions/index.js
@@ -10,31 +10,14 @@ export const filterStockInfos = filter => ({
 
 export const fetchStockInfos = () =>
 	dispatch =>
-		fetch('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%27http%3A%2F%2Fwww.emega.com.tw%2Fjs%2FStockTable.htm%27&format=json')
+		fetch('http://localhost:8000/stockInfos')
 			.then(response => response.json())
-			.then(json => {
-				let rawData = json.query.results.body.table[0].tbody.tr, tempMarket, tempMarketAlias, tempIndustry, stockInfos = []
+			.then(stockInfos => {
 
-				for (let i = 0; i < rawData[0].td.length; i+=2) {
-					rawData.map(tr => {
-						let first = tr.td[i], second = tr.td[i+1]						
-
-						if (first.content.trim()) {
-							stockInfos.push({
-								symbol: first.content.trim(),
-								name: second.content.trim(),
-								market: tempMarket,
-								marketAlias: tempMarketAlias,
-								industry: tempIndustry
-							})		 			
-							
-						} else {
-							tempMarket = first.b
-							tempMarketAlias = first.b == '上市' ? '.TW' : '.TWO'
-							tempIndustry = second.b
-						}
-					})	
-				} 
+				stockInfos.map(stockInfo => 
+					stockInfo['marketAlias'] = stockInfo['market'] == '上市' ? '.TW' : '.TWO'						
+				)		
+				
 				dispatch(addStockInfos(stockInfos))
 			})
 			.catch(ex => alert(`fetch error: ${ex.message}`))
