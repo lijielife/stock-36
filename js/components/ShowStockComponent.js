8cded0d20b79771d79c08320700f5448f6e2d39c
IT-kenlee(李昆芳) <markuslee0502@gmail.com>
2016-06-20 21:49:20 +0800

    Try use svg.

--- js/components/ShowStockComponent.js
+++ js/components/ShowStockComponent.js
@@ -1,12 +1,30 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
+import ReactDOM from 'react-dom'
 
-const ShowStockComponent = ({ stockPricesReducer }) => (
-	<div>
-		{stockPricesReducer.map(stockPrice => 
-			<div>{stockPrice.date}</div>
-		)}
-	</div>
-)
+class ShowStockComponent extends Component {
+	/*let lineGraph = []
+
+	if (stockPricesReducer.length) {
+		let count = 40, width = window.innerWidth / count, height = window.innerHeight / 2
+
+		for (let i = 0; i < count; i++) {
+			lineGraph.push(<rect x={width*i} width={width} height="20" stroke="green" style={{fill: 'rgb(0,0,255)'}} />)
+		}
+	}*/
+	
+	componentDidMount() {
+	    var width = ReactDOM.findDOMNode(this).clientWidth
+	    console.log(width)
+  	}
+
+	render() {
+		return (
+			<svg width="100%">
+				
+			</svg>
+		)
+	}
+}
 
 ShowStockComponent.propTypes = {
 	stockPricesReducer: PropTypes.array.isRequired
